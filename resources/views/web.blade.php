3e9071e8f3546f4be6baf3b393a72dddeb9f31d7
kalii8 <liva.karlivane@gmail.com>
2018-06-18 16:17:32 +0200

    Project Initial setup

---
+++ resources/views/web.blade.php
@@ -0,0 +1,23 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Cover Template for Bootstrap</title>
+
+  
+    <link href="../public/css/app.css" rel="stylesheet">
+   
+
+    <!-- Custom styles for this template -->
+  </head>
+
+  <body class="text-center">
+    <div id="app"> 
+      <script src="../public/js/app.js"></script>
+    </div>
+  </body>
+</html>
